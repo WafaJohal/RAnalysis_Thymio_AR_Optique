c34114d5d9320a06005945bfc6457340bb110c98
wafajohal <wafa.johal@epfl.ch>
2017-06-21 17:20:42 +0200

    first plots

---
+++ util.R
@@ -0,0 +1,138 @@
+
+library(ggplot2)
+#prefix = "~/Documents/CODING/Data_Analysis/cellulo_log_analysis/haptics-experiment/analysis/"
+#prefix = "/Users/wafajohal/Documents/CODING/DATA_ANALYSIS/cellulo-experiment-logs/haptics-experiment/analysis/"
+prefix = "~/Documents/CODING/DATA_ANALYSIS/cellulo-experiment-logs/haptics-experiment/analysis/"
+theme_Publication <- function(base_size=14, base_family="Helvetica") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.2), hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.1),
+            axis.text = element_text(), 
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.2, "cm"),
+            legend.margin = unit(0.1, "cm"),
+            legend.title = element_text(face="italic"),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+scale_colour_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+scale_fill_levels_discrete <- function(...){
+  library(scales)
+  discrete_scale("fill","Publications",manual_pal(values = c("#80b240","#589C00","#3b5913","#293a13")), ...)
+}
+
+scale_fill_levels_continous <- function(...){
+  library(scales)
+  continous_scale("fill","leveld",low="#80b240", high = "#293a13")
+}
+
+low_level = "#80b240"
+high_level = "#293a13"
+scale_fill_levels_gradient = scale_fill_gradient(low = low_level, high = high_level)
+
+
+
+# lows 94ed21 8bb257 87d323 7aa83e 80b240 3b5913
+# highs 293a13 589C00 233808
+# 4levels 80b240 589C00 3b5913 293a13
+
+scale_colour_levels_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+}
+
+discrete_var_pal = c("#33658a","#f26419","#f6ae2d","#86bbd8","#8da3b5","#193244")
+
+scale_fill_activities_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = discrete_var_pal), ...)
+}
+
+scale_colour_activities_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = discrete_var_pal), ...)
+}
+
+scale_fill_V1_Publication <- function(...){
+  library(scales) 
+  discrete_scale("fill","Publication",manual_pal(values = c("#3c1518","#69140e","#a44200","#d58936","#fffb46")), ...)
+}
+
+scale_colour_V1_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+}
+
+
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
